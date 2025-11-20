843afa425949e67f4ea5a2ef078b59e419a55d29
codewhat7 <feiweng579@gmail.com>
2025-11-20 15:06:35 +0800

    Log Screen deleted

--- app/src/main/java/com/example/smartfit/ui/log/LogScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.smartfit.ui.log
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun LogScreen() {
-    Text(text = "Activity Log Screen")
-}
