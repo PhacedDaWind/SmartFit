552a2013ecf06c6f23b879f6845a5139714bf05e
codewhat7 <feiweng579@gmail.com>
2025-11-21 23:25:07 +0800

    for ai response from gemini? maybe?

---
+++ app/src/main/java/com/example/smartfit/data/local/ExerciseDto.kt
@@ -0,0 +1,9 @@
+package com.example.smartfit.data.local
+
+data class ExerciseDto(
+    val name: String,
+    val type: String,
+    val muscle: String,
+    val difficulty: String,
+    val instructions: String
+)
